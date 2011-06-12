5e5c9920a6a9fb7cf63b55227052fa1976e692c7
Vlad Bokov <razum2um@mail.ru>
2011-06-13 03:24:49 +0700

    add some docs

--- app/router.js
+++ app/router.js
@@ -1,13 +1,13 @@
-var stdRouter = require("../lib/router");
+var stdRouter = require("../lib/router"); // required: this router is default
 
 var nestedApp = require("./nested/router");
 var views = require("./views");
 
 var urls = {
-    "/": views.start,
+    "/": views.start, // required: function, which handles this route "/app/"
     "start": views.start,
-    "nested": nestedApp.route,
+    "nested": nestedApp.route,  // optional: function, which is defined into nested app, 
+                                //which is given there an urls-Object
 }
 
-exports.route = stdRouter.route(urls);
-
+exports.route = stdRouter.route(urls); // required: defition & export of `route` from this scope
