a9c4f84627629f76617588ecb68bc67672470256
Vlad Bokov <razum2um@mail.ru>
2011-06-13 03:18:11 +0700

    correct routing

--- app/nested/router.js
+++ app/nested/router.js
@@ -1,12 +1,10 @@
-var stdRouter = require("../lib/router");
+var stdRouter = require("../../lib/router");
 
-var nestedApp = require("./nested/router");
 var views = require("./views");
 
 var urls = {
     "/": views.start,
     "start": views.start,
-    "nested": nestedApp.route,
 }
 
 exports.route = stdRouter.route(urls);
--- app/nested/views.js
+++ app/nested/views.js
@@ -1,7 +1,7 @@
-exports.start = function (request, response, urlParams) {
+exports.start = function(request, response, urlParams) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("Request handler 'start' from '" + __filename + "' was called.");
   response.end();
-  console.log("Request handler 'start' '" + __filename + "' was called.");
+  console.log("Request handler 'start' from '" + __filename + "' was called.");
 }
 
