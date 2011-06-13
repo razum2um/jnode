a97bfefd14b1f91697705e4cff9ff00a5367bdf6
Vlad Bokov <razum2um@mail.ru>
2011-06-13 20:57:06 +0700

    rewrite nested router rules

--- router.js
+++ router.js
@@ -4,16 +4,18 @@ var views = require("./views");
 var appRouter = require("./app/router");
 
 var urls = {
-    "/": views.start,
-    "start": views.start,
+    "/": views.start, // required definition "/"
+    "app": appRouter, // optional give a nested router
+    "start": views.start, // simple view-functions
     "upload": views.upload,
     "retrospect": views.retrospect,
     "ls": views.ls,
     "mpd": views.mpd,
     "media": views.media,
     "date": views.date,
-    "app": appRouter.route,
 }
 
-exports.route = stdRouter.route(urls);
+// required, hardcoded for every `router`
+// sad, but you MUST export & call function `route`
+exports.route = stdRouter.route(urls); 
 
