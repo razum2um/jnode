12f2c535db1367ccb19534ff4fe75a2f2f1d10d5
Vlad Bokov <razum2um@mail.ru>
2011-06-13 22:04:06 +0700

    cleanup

--- app/nested/router.js
+++ app/nested/router.js
@@ -3,8 +3,8 @@ var stdRouter = require("../../lib/router");
 var views = require("./views");
 
 var urls = {
-    "start": views.start,
-    ".*": views.start,
+    "^start": views.start,
+    "^$": views.start,
 }
 
 exports.route = stdRouter.route(urls);
