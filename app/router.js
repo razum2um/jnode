a4823a035cdeb304171e241882df5ccfbc302d49
Vlad Bokov <razum2um@mail.ru>
2011-06-13 21:00:34 +0700

    some comments

--- app/router.js
+++ app/router.js
@@ -6,8 +6,8 @@ var views = require("./views");
 var urls = {
     "/": views.start, // required: function, which handles this route "/app/"
     "start": views.start,
-    "nested": nestedApp.route,  // optional: function, which is defined into nested app, 
-                                //which is given there an urls-Object
+    "nested": nestedApp,  // optional: function, which is defined into nested app, 
+                          //which is given there an urls-Object
 }
 
 exports.route = stdRouter.route(urls); // required: defition & export of `route` from this scope
